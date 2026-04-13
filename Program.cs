1bfd4d273de5751acfa10069f838e8cb38a80826
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2026-04-13 10:00:46 +0200

    🙈 features/ add gitignore

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome in EasySave!");
+Console.WriteLine("Enter the source folder path :");
+
+string SourceFolderPath = Console.ReadLine()!;
+
+Console.WriteLine("Source Folder parth is " + SourceFolderPath);
