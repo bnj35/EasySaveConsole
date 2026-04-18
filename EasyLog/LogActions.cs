cfc7929027820950f49abb3be74cc467350d7f49
Antoine Roure <antoine.roure@outlook.fr>
2026-04-18 15:42:49 +0200

    ✨ feature/added logger and restructured project

---
+++ EasyLog/LogActions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class LogActions
+{
+    public const string FILE_COPY = "FILE_COPY";
+    public const string DIRECTORY_CREATION = "DIRECTORY_CREATION";
+}
