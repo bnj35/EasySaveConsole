efba5e9737fe871863db47737e713d7cbcb59b01
Antoine Roure <antoine.roure@outlook.fr>
2026-04-21 17:37:03 +0200

    🩹 feature/logs refactoring

--- EasyLog/LogActions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-public static class LogActions
-{
-    public const string FILE_COPY = "FILE_COPY";
-    public const string DIRECTORY_CREATION = "DIRECTORY_CREATION";
-}
